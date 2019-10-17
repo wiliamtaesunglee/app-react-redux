46b9f46da0e1e31c8e19cbe7cb5f0fa3410d5ef5
wil.i.am <wiliam@wiliams-MacBook-Pro.local>
2019-10-17 09:33:19 -0300

    alert state course title test

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
 class CoursesPage extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
+    
+    state = {
             course: {
                 title: ''
             }
         }
-    }
-
+    
     handleChange = (event) => {
         const course = {...this.state.course, title: event.target.value}
         this.state({course: course})
     }
 
-
+    handleSubmit = (event) => {
+        event.preventDefault()
+        alert(this.state.course.title)
+    }
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h2>Courses</h2>
                 <h3>Add Course</h3>
                 <input
